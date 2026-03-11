21477887111eacbb7c26b4aabdda1713dde7eb65
Dharani <dharani@Dharanis-MacBook-Air.local>
2026-03-11 17:29:36 +0530

    login logout  implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,19 @@ import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
 import PageLoader from './components/PageLoader'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
-  const { checkAuth, isCheckingAuth, authUser} = useAuthStore()
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore()
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
-  
+
   console.log({ authUser });
-  
-  if(isCheckingAuth) return <PageLoader/>
-  
+
+  if (isCheckingAuth) return <PageLoader />
+
   //login()
   return (
     <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
@@ -25,14 +25,14 @@ const App = () => {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-      <button type='button' className='z-10 btn btn-primary'>Login</button>
-          <Routes>
-        <Route path='/' element={authUser ? <ChatPage/>: <Navigate to={'/login'}/>}></Route>
-        <Route path='/login' element={!authUser ? <LoginPage/>: <Navigate to={'/'}/>}></Route>
+      {/* <button type='button' className='z-10 btn btn-primary'>Login</button> */}
+      <Routes>
+        <Route path='/' element={authUser ? <ChatPage /> : <Navigate to={'/login'} />}></Route>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={'/'} />}></Route>
         <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={'/'} />}></Route>
-      </Routes> 
-      
-      <Toaster/>
+      </Routes>
+
+      <Toaster />
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -9,36 +9,68 @@ export const useAuthStore = create((set) => (
         authUser: false,
         isCheckingAuth: true,
         isSigningUp: false,
+        isLoggingIn: false,
 
         checkAuth: async () => {
             try {
                 const res = await axiosInstance('http://localhost:3000/api/auth/check')
-                set({ authUser: res.data})
+                set({ authUser: res.data })
             } catch (error) {
                 console.log('Error in auth check: ', error);
                 set({ authUser: null })
-            
+
             } finally {
-                set({isCheckingAuth: false})
-          }
+                set({ isCheckingAuth: false })
+            }
         },
 
         signup: async (data) => {
             set({ isSigningUp: true })
             try {
                 //console.log(axiosInstance);
-                
+
                 const res = await axiosInstance.post('http://localhost:3000/api/auth/signup', data)
                 set({ authUser: res.data })
-                
+
                 toast.success('Account created successfully')
             } catch (error) {
                 console.log('Error in signup: ', error);
                 toast.error(error.response.data.message)
             } finally {
-                set({isSigningUp: false})
+                set({ isSigningUp: false })
             }
+        },
+
+        login: async (data) => {
+            set({ isLoggingIn: true })
+            try {
+                //console.log(axiosInstance);
+                const res = await axiosInstance.post('http://localhost:3000/api/auth/login', data)
+                set({ authUser: res.data })
+
+                toast.success('Logged in successfully')
+            } catch (error) {
+                console.log('Error in login: ', error);
+                toast.error(error.response.data.message)
+            } finally {
+                set({ isLoggingIn: false })
+            }
+        },
+
+        logout: async () => {
+            try {
+              
+                await axiosInstance.post('http://localhost:3000/api/auth/logout')
+                set({ authUser: null })
+                toast.success('Logged out successfully')
+            } catch (error) {
+                console.log('Error logging out: ', error);
+                
+            toast.error('Error occurred while logging out')
+          }
         }
         
+
+
     }
 ))
